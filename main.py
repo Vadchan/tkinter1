628ee7f8e99a179f927dd123957e4d5a46cc16f7
Vadchan <vadimzavyalov002@gmail.com>
2022-03-31 11:47:18 +0300

    Выводит значение в окно, которое взял из поисковой строки

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ class About(tk.Toplevel):
         self.button.place(x=860, y=900, width=140, height=35)
 
 class StartWindow(tk.Tk):
+
+
     def __init__(self):
         super().__init__()
         self.geometry('1920x1080')
@@ -21,8 +23,10 @@ class StartWindow(tk.Tk):
         self.label = tk.Label(text="Выборка", font=("Arial Bold", 25))
         self.label.grid(column=0, row=0)
         #Поисковая сторка
-        self.txt = tk.Entry(width=30)
-        self.txt.place(x=200, y=200, width=1500, height=35)
+        var3 = tk.StringVar()
+        self.txt = tk.Entry(width=30, textvariable=var3)
+        #self.name = var3.get()
+        self.txt.place(x=200, y=200, width=700, height=35)
         self.txt.focus()
         self.label.pack(padx=20,pady=20)
         #Режимы сортировки
@@ -34,14 +38,18 @@ class StartWindow(tk.Tk):
         self.btn = tk.Button(self, text="Начать", command=self.open_window)
         self.btn.place(x=700, y=250, width=500, height=25)
 
+
+
         #Вывод в поле
         def insert_text():
           #s = "print(Meta1.shape)"
-          self.text.insert(1.0, f'{Meta1}\n')
+          #self.text.insert(1.0, f'{Meta1}\n')
+          self.text.insert(2.0, f'{var3.get()}')
+
 
         #Размер рамки
         self.text = tk.Text(width=232, height=30, bg="darkgreen", fg='white')
-        self.text.place(x=20, y=500)
+        self.text.place(x=20, y=300)
         self.frame = tk.Frame()
         self.frame.pack()
         self.b = tk.Button(self, text="Вставить", command=insert_text)
@@ -57,6 +65,7 @@ class StartWindow(tk.Tk):
         about.grab_set()
     def print(self):
         pass
+
 
 if __name__ == "__main__":
     app = StartWindow()
