46ff85668e837c41710af38e241fb9bca9405d12
Vadchan <vadimzavyalov002@gmail.com>
2022-03-31 12:57:19 +0300

    Удалил ненужные столбцы в таблице

--- csv_module.py
+++ csv_module.py
@@ -1,14 +1,35 @@
-import matplotlib.pyplot as plt
+import  matplotlip as plt
+#import matplotlib.pyplot as plt
 from pandas import read_csv
 
+import pandas as pd
 
-Meta1 = read_csv('metacritic-20141019-152743.csv')
-print(Meta1["platform"].value_counts().head())
-print('Атрибуты файла')
-print(Meta1.head())
-print(Meta1.shape)
+Meta1 = pd.read_csv('metacritic-20141019-152743.csv')
+#Удаление не нужных столбцов
+Meta1 = Meta1.drop(columns=['link', 'genre', 'platform'])
+print('Атрибуты ИГРЫ')
+#print(Meta1.head())
+#print(Meta1.shape)
 #s=print(Meta1.shape)
 #print(s)
+
+# Сброс ограничений на количество выводимых рядов
+pd.set_option('display.max_rows', 7)
+
+# Сброс ограничений на число столбцов
+pd.set_option('display.max_columns', None)
+
+# Сброс ограничений на количество символов в записи
+pd.set_option('display.max_colwidth', None)
+
+
+result = Meta1[(Meta1['title'] == 'Diablo')]
+print(result)
+
+
+
+
+
 
 '''
 from tkinter import *
