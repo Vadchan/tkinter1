4ea13eafb514eada4aa4f36d01a5cba2c1f3419e
Vadchan <vadimzavyalov002@gmail.com>
2022-04-05 11:28:39 +0300

    прописывал логику поведения

--- csv_module.py
+++ csv_module.py
@@ -1,17 +1,49 @@
 import  matplotlip as plt
 #import matplotlib.pyplot as plt
-from pandas import read_csv
-
 import pandas as pd
+
 
 Meta1 = pd.read_csv('metacritic-20141019-152743.csv')
 #Удаление не нужных столбцов
 Meta1 = Meta1.drop(columns=['link', 'genre', 'platform'])
-print('Атрибуты ИГРЫ')
-#print(Meta1.head())
-#print(Meta1.shape)
-#s=print(Meta1.shape)
-#print(s)
+#print('Атрибуты ИГРЫ')
+
+pd.set_option('display.max_rows', 3)
+print('Все игры')
+print((Meta1['title']))
+print('^^Все игры^^')
+
+class CSV():
+
+    def search():
+        a = input('Введите название игры: ')
+        result = Meta1[(Meta1['title'] == str(a))]
+        print('Вся информация о вашей игре')
+        print(result)
+        print('Хотите воспользоваться нахождением максимально похожей игры?')
+        print('да')
+        print('нет')
+        b = input()
+        w = result['genre_tags']
+        print(w)
+
+        if b == str('да'):
+            rr = input('введите жанр игры, что указан выше: ')
+            sss = Meta1[(Meta1['genre_tags'] == str(rr))]
+            ss= sss.drop(columns=['user_score', 'publisher', 'critics_reviews_count', 'release_date',
+                                  'maturity_rating',  'metascore',  'user_reviews_count', 'developer'])
+            print(ss)
+            print('Желаете отсортировать игры по оценке metascore?')
+            print('да')
+            print('нет')
+            z = input()
+            if z == str('да'):
+                print('agnionwg')
+
+
+
+
+
 
 # Сброс ограничений на количество выводимых рядов
 pd.set_option('display.max_rows', 7)
@@ -24,7 +56,7 @@ pd.set_option('display.max_colwidth', None)
 
 
 result = Meta1[(Meta1['title'] == 'Diablo')]
-print(result)
+#print(result)
 
 
 
@@ -55,3 +87,5 @@ label.pack()
 
 root.mainloop()
 '''
+if __name__ == "__main__":
+    CSV.search()
