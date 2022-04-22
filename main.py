d47844c315e4d1c3358d9ea11ae0d85f04bcd459
Vadchan <vadimzavyalov002@gmail.com>
2022-04-22 12:11:03 +0300

    было сделано окно №5, пользователь выбирает диапозон оценки по user score, так же жанр желаемых игр

--- main.py
+++ main.py
@@ -28,15 +28,19 @@ class StartWindow(tk.Tk):
 
         #Кнопка перехода во второе окно
         self.btn = tk.Button(self, text="Начать", bg="black", fg="red", command=self.open_window)
-        self.btn.place(x=60, y=178, width=550, height=25)
+        self.btn.place(x=60, y=184, width=550, height=20)
 
         #Кнопка перехода в третье окно
-        self.btn = tk.Button(self, text="Пользователь", bg="black", fg="red", command=self.window3)
-        self.btn.place(x=60, y=208, width=550, height=25)
+        self.btn = tk.Button(self, text="Пользователь по metascore", bg="black", fg="red", command=self.window3)
+        self.btn.place(x=60, y=228, width=550, height=20)
 
         # Кнопка перехода в четвёртое окно
         self.btn = tk.Button(self, text="Вся информация о таблице", bg="black", fg="red", command=self.information)
-        self.btn.place(x=60, y=272, width=550, height=25)
+        self.btn.place(x=60, y=272, width=550, height=20)
+
+        # Кнопка перехода в пятое окно
+        self.btn = tk.Button(self, text="Пользователь по user score", bg="black", fg="red", command=self.win5)
+        self.btn.place(x=60, y=206, width=550, height=20)
 
 
 
@@ -60,7 +64,102 @@ class StartWindow(tk.Tk):
 
         #Кнопка записи в тестовое поле
         self.b = tk.Button(self, text="Вся информация о игре", bg="black", fg="red", command=insert_text)
-        self.b.place(x=60, y=240, width=550, height=25)
+        self.b.place(x=60, y=250, width=550, height=20)
+
+
+    def win5(self):
+        # Зарываем предыдущее окно
+        self.destroy()
+        # Создание нового окна
+        soon = tk.Tk()
+        soon.title("Третье окно")
+        soon.geometry("1300x500")
+        soon.configure(bg='#856ff8')
+        soon.lbl = tk.Label(soon, text="Первое значение: ", background="#856ff8", font=("Arial Bold", 10))
+        soon.lbl.place(x=20, y=10)
+        soon.lbl = tk.Label(soon, text="Второе значение: ", background="#856ff8", font=("Arial Bold", 10))
+        soon.lbl.place(x=20, y=40)
+        soon.lbl = tk.Label(soon, text="Жанр игры: ", background="#856ff8", font=("Arial Bold", 10))
+        soon.lbl.place(x=20, y=70)
+        # Первое значение
+        soon.combo1 = ttk.Combobox()
+        soon.combo1['values'] = (
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+            29, 30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+            58,
+            59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
+            86,
+            87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100)
+
+        soon.combo1.current(0)  # установите вариант по умолчанию
+        soon.combo1.place(x=130, y=10, width=40, height=25)
+
+        # Второе значение
+        soon.combo2 = ttk.Combobox()
+        soon.combo2['values'] = (
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+            29,
+            30,
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+            58,
+            59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
+            86,
+            87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100)
+
+        soon.combo2.current(80)  # установите вариант по умолчанию
+        soon.combo2.place(x=130, y=40, width=40, height=25)
+
+        # Жанр игр
+        soon.combo3 = ttk.Combobox()
+        soon.combo3['values'] = ('Real-Time Strategy', 'Action RPG', 'Sim', 'Arcade', 'Modern Jet',
+                                 'Other Shooters', 'Historic', 'Turn-Based Strategy', 'Sci-Fi', 'Puzzle',
+                                 'Adventure Games', '3D', 'General', '2D', 'Modern', 'Platformers',
+                                 'Miscellaneous', 'Fantasy', 'Other Strategy Games', 'Role-Playing',
+                                 'Formula One', 'GT / Street', 'Stock Car', 'Street', 'Racing',
+                                 'Rally / Offroad', 'Motocross', 'Kart', 'Futuristic', 'Small Spaceship',
+                                 'Futuristic Combat Sims', 'Large Spaceship', 'Futuristic Jet', 'WWII',
+                                 'Combat Sims', 'Tank', 'Ship', 'Civilian Plane', 'Train', 'Submarine', 'Tycoon',
+                                 'First-Person Shooters', 'Tactical Shooters', 'Action', 'Wrestling',
+                                 "Beat-'Em-Up", 'Scrolling', 'Rail', 'Static', 'Light Gun', 'Shooter',
+                                 'First-Person', 'MOBA', 'Compilation', 'Fighting Games', 'Music',
+                                 'Simulations', 'Helicopter', 'Military', 'WWI', 'Old Jet', 'Fighting',
+                                 'Action Adventure', 'Interactive Movie', 'Horror', 'Tennis', 'Soccer',
+                                 'Management', 'Other Sports Games', 'Baseball', 'Basketball', 'Surfing',
+                                 'Alternative Sports', 'Olympic Sports', 'Snowboarding', 'Golf', 'Other',
+                                 'Skateboarding', 'Biking', 'Billiards', 'Volleyball', 'Hunting', 'Rugby',
+                                 'Football', 'Bowling', 'Hockey', 'Truck', 'On-foot', 'Virtual Life',
+                                 'Futuristic Sub', 'Mech', 'Breeding/Constructing', 'PC-style RPG',
+                                 'Massively Multiplayer', 'Console-style RPG')
+
+        soon.combo3.current(0)  # установите вариант по умолчанию
+        soon.combo3.place(x=93, y=70, width=150, height=25)
+
+        text = tk.Text(width=100, height=30, bg="#4169E1", fg='white')
+        text.pack()
+        text.insert(1.0, f"Первое значение: {soon.combo1.get()}\n")
+        text.insert(2.0, f'Второе значение:{soon.combo2.get()}\n')
+
+        def callbackFunc1(event):
+            text.delete("1.0", "end")
+            text.insert(1.0, f"Первое значение: {soon.combo1.get()}\n")
+
+        soon.combo1.bind("<<ComboboxSelected>>", callbackFunc1)
+
+        def callbackFunc2(event):
+            text.insert(2.0, f"Второе значение: {soon.combo2.get()}\n")
+
+        soon.combo2.bind("<<ComboboxSelected>>", callbackFunc2)
+
+        def callbackFunc3(event):
+            text.insert(3.0, f"Жанр: {soon.combo3.get()}\n")
+            text.insert(4.0, csv_module.son1(soon.combo1.get(), soon.combo2.get(), soon.combo3.get()))
+
+        soon.combo3.bind("<<ComboboxSelected>>", callbackFunc3)
+
+        esc = tk.Button(soon, text="Начальный экран", bg="black", fg="red",
+                        command=lambda: [soon.destroy(), StartWindow()])
+        esc.place(x=20, y=100, width=160, height=35)
 
 
     def open_window(self):
